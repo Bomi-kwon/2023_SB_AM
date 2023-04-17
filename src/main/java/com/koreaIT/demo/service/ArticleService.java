9fee76d47fd1b1a2bdc462e64a64af3e1cc23efa
Bomi-kwon <bbomicoding@gmail.com>
2023-04-17 13:48:52 +0900

    mybatis, mysql-connector, repository mapper

--- src/main/java/com/koreaIT/demo/service/ArticleService.java
+++ src/main/java/com/koreaIT/demo/service/ArticleService.java
@@ -15,8 +15,6 @@ public class ArticleService {
 	@Autowired
 	public ArticleService(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
-		
-		articleRepository.makeTestData();
 	}
 
 	public Article writeArticle(String title, String body) {
