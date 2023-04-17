91e7f496b8c5d34ecf83817bfd8f860a5ace9654
Bomi-kwon <bbomicoding@gmail.com>
2023-04-17 10:42:54 +0900

    TestData 만들기

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -18,6 +18,22 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for(int i = 1 ; i <= 10 ; i++) {
+			
+			int id = this.lastArticleId + 1;
+			this.lastArticleId = id;
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			Article article = new Article(id, title, body);
+			articles.add(article);
+		}
 	}
 
 	@RequestMapping("/usr/article/doAdd")
